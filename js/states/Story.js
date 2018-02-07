d9e450ebe33854e2a668c295ab856ecc6945a106
asegert <zannagal@live.ca>
2018-02-07 10:57:22 -0800

    Text Processing Commit
    Completed the text processing/display for story page explaining the 'mission'.

--- js/states/Story.js
+++ js/states/Story.js
@@ -32,6 +32,7 @@ Locator.StoryState = {
         this.agent.destroy();
         
         //Add Story here
+        this.storyText();
     },
     storyText: function()
     {
@@ -41,24 +42,88 @@ Locator.StoryState = {
         //3_ = him/her
         //2_ = he/she
         //1_ = number
-        var text = this.processText(this.gameData.StoryText[0], Locator.gender);
+        //4- = himself/herself
+        //3- = Him/Her
+        //2- = He/She
+        var text = this.processText(this.gameData.StoryText[0], Locator.gender, 95, "") + "\n";
+        for(var i = 0, len = this.gameData.rounds.length; i<len; i++)
+        {
+            text = text + "\n\t\t\t\t\t\t\t\t-" + this.processText(this.gameData.rounds[i].storyText, Locator.gender, 90, "\t\t\t\t\t\t\t\t");
+        }
+        text = text + "\n\n" + this.processText(this.gameData.StoryText[1], Locator.gender, 95, "");//Maybe Center, large, buttonize?
+        
+        this.add.text(0, 50, text, {font: '24px'});
     },
-    processText: function(text, gender)
+    processText: function(text, gender, spaceLen, indent)
     {
-        var num = this.gameData.Levels.length;
+        var name = this.gameData.nameM;
+        var subject = "he";
+        var object = "him";
+        var personal = "himself";
+        var num = this.gameData.rounds.length;
         
         if(gender == "male")
         {
-            var name = this.gameData.nameM;
-            var subject = "he";
-            var object = "him";
+            name = this.gameData.nameF;
+            subject = "she";
+            object = "her";
+            personal = "herself";
+        }
+        
+        text = text.replace(/----/g , personal);
+        text = text.replace(/---/g , this.capitalizeFirstLetter(object));
+        text = text.replace(/--/g , this.capitalizeFirstLetter(subject));
+        //text = text.replace(/_/g , num);
+        text = text.replace(/____/g , name);
+        text = text.replace(/___/g , object);
+        text = text.replace(/__/g , subject);
+        text = text.replace(/_/g , num);
+        
+        var index = 0;
+        var tempText = text;
+        var returnText = "";
+        if((index + spaceLen) > text.length)
+        {
+            returnText = returnText + tempText;
         }
         else
         {
-            var name = this.gameData.nameF;
-            var subject = "she";
-            var object = "her";
+            while((index + spaceLen) < text.length)
+            {
+                var lastIndex = index;
+                index = index + spaceLen;
+                var workingText = tempText.substring(lastIndex, index);
+            
+                if(tempText.charAt(index) === " ")
+                {
+                    workingText = workingText + "\n";
+                    index = index+1;
+                }
+                else
+                {
+                    //process back to nearest space
+                    var tempIndex = workingText.lastIndexOf(" ");
+                    workingText = tempText.substring(lastIndex, (lastIndex + tempIndex)) + "\n";
+                    index = (lastIndex + tempIndex + 1);
+                }
+            
+                if(lastIndex == 0)
+                {
+                    returnText = returnText + workingText;
+                }
+                else
+                {
+                    returnText = returnText + indent + workingText;
+                }
+            }
+            returnText = returnText + indent + tempText.substring(index);
         }
+        
+        return returnText;
+    },
+    capitalizeFirstLetter: function(string) 
+    {
+        return string.charAt(0).toUpperCase() + string.slice(1);
     },
     proceed: function()
     {
